541d9e73e8d8c0582640b8b17b993e962e15209a
Godones <1925466036@qq.com>
2024-08-19 20:34:44 +0800

    add get_must for spin::Once

--- basic/src/sync/mod.rs
+++ basic/src/sync/mod.rs
@@ -1,1 +1,12 @@
 pub use ksync::{Mutex, MutexGuard, RwLock};
+pub use spin::Once;
+
+impl<T> OnceGet<T> for Once<T> {
+    fn get_must(&self) -> &T {
+        unsafe { self.get_unchecked() }
+    }
+}
+
+pub trait OnceGet<T> {
+    fn get_must(&self) -> &T;
+}
